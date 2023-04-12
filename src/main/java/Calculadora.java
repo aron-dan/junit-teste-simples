f371502437c246d55faa2bcb1194fc9841dae3d6
aron-dan <82916233+aron-dan@users.noreply.github.com>
2023-04-11 22:16:01 -0300

    Final do projeto

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,39 @@
+
+public class Calculadora {
+	
+	public double Soma() {
+		double a , b;
+		double result;
+		a = 10;
+		b = 10;
+		result = a+b;
+		return result;
+	}
+	
+	public double Subtracao() {
+		double a , b;
+		double result;
+		a = 10;
+		b = 10;
+		result = a-b;
+		return result;
+	}
+	
+	public double Divisao() {
+		double a , b;
+		double result;
+		a = 10;
+		b = 10;
+		result = a/b;
+		return result;
+	}
+	public double Potenciacao() {
+		double a , b;
+		double result;
+		a = 10;
+		b = 3;
+		result = Math.pow(a, b);;
+		return result;
+	}
+
+}
