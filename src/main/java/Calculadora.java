ec9bf283ac80f4c3807b42f5c6bd70fdf8f334c2
aron-dan <82916233+aron-dan@users.noreply.github.com>
2023-04-25 20:23:04 -0300

    Correcao da variavel result

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -4,9 +4,16 @@ public class Calculadora {
 	public double Soma(double a, double b) {
 		return a + b;
 	}
+<<<<<<< HEAD
 
+=======
+>>>>>>> 247c4c5 (correção da variavel result)
 	public double Subtracao(double a, double b) {
 		return a - b;
+	}
+	
+	public double Multiplicacao(double a, double b) {
+		return a*b;
 	}
 	
 	public double Divisao(double a, double b) {
--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -6,19 +6,20 @@ import org.junit.jupiter.api.Test;
 
 class CalculadoraTest {
 
+	Calculadora calculadora = new Calculadora();
+
 	@Test
-	 public void testeSoma() {
-		//Arrange
-		Calculadora calculadora = new Calculadora();
-		
-		//Act
-		double resultado = calculadora.Soma(10,10);
-		
-		//Assert
+	public void testeSoma() {
+		// Act
+		double resultado = calculadora.Soma(10, 10);
+
+		// Assert
 		Assertions.assertEquals(resultado, calculadora.Soma(resultado, 0));
 	}
-	
+
+	@Test
 	public void testeSubtracao() {
+<<<<<<< HEAD
 		//Arrange
 		Calculadora calculadora = new Calculadora();
 
@@ -27,9 +28,27 @@ class CalculadoraTest {
 
 		//Assert
 		Assertions.assertEquals(result, calculadora.Subtracao(result, result));
+=======
+		// ACT
+		double result = calculadora.Subtracao(20, 30);
+
+		// Assert
+		Assertions.assertEquals(result, calculadora.Subtracao(result, 0));
+>>>>>>> 247c4c5 (correção da variavel result)
 	}
-	
+
+	@Test
+	public void TesteMultiplicacao() {
+		// ACT
+		double result = calculadora.Multiplicacao(20, 30);
+
+		// Assert
+		Assertions.assertEquals(result, calculadora.Multiplicacao(result, 1));
+	}
+
+	@Test
 	public void testeDivisao() {
+<<<<<<< HEAD
 		//Arrange
 		Calculadora calculadora = new Calculadora();
 
@@ -38,9 +57,18 @@ class CalculadoraTest {
 
 		//Assert
 		Assertions.assertEquals(result, calculadora.Divisao(result, result));
+=======
+		// ACT
+		double result = calculadora.Divisao(20, 30);
+
+		// Assert
+		Assertions.assertEquals(result, calculadora.Divisao(result, 1));
+>>>>>>> 247c4c5 (correção da variavel result)
 	}
-	
+
+	@Test
 	public void testePotencia() {
+<<<<<<< HEAD
 		//Arrange
 		Calculadora calculadora = new Calculadora();
 
@@ -49,7 +77,13 @@ class CalculadoraTest {
 
 		//Assert
 		Assertions.assertEquals(result, calculadora.Potenciacao(result, result));
+=======
+		// ACT
+		double result = calculadora.Potenciacao(20, 30);
+
+		// Assert
+		Assertions.assertEquals(result, calculadora.Potenciacao(result, 1));
+>>>>>>> 247c4c5 (correção da variavel result)
 	}
-	
 
 }
